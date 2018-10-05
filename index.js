545cf8df76d77faefbcd4b49d0e93db10f432f39
Anthony Pagano <apagano51@gmail.com>
2018-10-04 20:47:39 -0400

    master: initial commit of quiz app from replit

---
+++ index.js
@@ -0,0 +1,146 @@
+"use strict";
+
+// set counters for questions and the score
+let questionCount = 0;
+let scoreCount = 0;
+
+// increment the question counter
+function questionCounter() {
+  questionCount++;
+}
+
+// increment the score counter
+function scoreCounter() {
+  scoreCount++;
+}
+
+// function to start the quiz from the landing page which will call for the question and start the quiz engine
+function startQuiz() {
+  $('#startTheQuiz').click(function(event) {
+    nextQuestion();
+    quizEngine();
+  });
+}
+
+// function to move to and load the next question
+function nextQuestion() {
+  $('.container').html(loadQuestions());  
+}
+
+// load the necessary question info and assets from the STORE array
+function loadQuestions() {
+  $('.container').html(`
+    <section id="quizQuestions" role="region">
+    <img src="${STORE[questionCount].relatedPhoto}" alt="${STORE[questionCount].alt}" />
+    <ul>
+      <li class="question">Question ${questionCount +1} of 10</li>
+      <li class="score">Score ${scoreCount} of 10</li>
+    </ul>    
+    <h2>${STORE[questionCount].question}</h2>
+    <form id="js-quizQuestionForm" role="form">
+      <fieldset class="fieldset">
+  `);  
+  
+  const countTo = 3;
+  for (let i = 0; i <= countTo; i++) {
+    $('.fieldset').append(`
+          <label>
+            <input type="radio" name="answer" value="${STORE[questionCount].answerOptions[i]}" required></input>
+            <span>${STORE[questionCount].answerOptions[i]}</span>
+          </label>
+    `);
+  }    
+
+  $('.fieldset').append(`
+      </fieldset>  
+      <button type="submit">Submit</button>
+    </form>      
+  </section>
+  `);
+}
+
+// check the users selected answer, compare it to what is in the STORE array for the question shown. If it is correct run the function to show them the correct answer pannel, if it's incorrect show them the incorrect answer pannel
+function checkTheAnswer() {
+  $('#js-quizQuestionForm').submit(function(event) {
+    event.preventDefault();
+    let userChoice = $('input:checked');
+    let answer = userChoice.val();
+    let correctAnswer = `${STORE[questionCount].theRightAnswer}`;
+    showAnswerPannel(answer, correctAnswer)
+  });
+}
+
+// takes the users answer and the correct answer and passes them in to show the user the correct or incorrect user pannel. In the case of a correct answer the correct answer is shown and the score increment function is called. In the case of an incorrect answer the users answer as well as the correct answer are shown. For either answer the increment question function is called.
+function showAnswerPannel(answer, correctAnswer) {
+  let correctAnswerPhrase = `${STORE[questionCount].rightAnswerPhrase}` + `${STORE[questionCount].theRightAnswer}`;
+  $('.container').html(`
+      <section role="region">
+  `);  
+
+  if (answer === correctAnswer) {
+    scoreCounter();
+    $('.container').append(`
+        <img src="https://s3.amazonaws.com/speedsport-news/speedsport-news/wp-content/uploads/2018/04/09082815/80-SteveTorrence-Celeb-LasVegas1-1068x799.jpg" alt="Steve Torrence wins the top fuel class at the 4 Wide Nationals in Las Vegas" />
+          <p>You Are Correct!</p>
+          <p>${correctAnswerPhrase}.</p>
+    `);    
+  } else {
+    $('.container').append(`
+      <img src="https://media.giphy.com/media/HiqKxcqQn5OF2/giphy.gif" alt="Ron Capps' engine explodes" />
+        <p>You chose ${answer}.</p>
+        <p>I'm sorry, your answer is incorrect.</p>
+        <p>The correct answer is ${STORE[questionCount].theRightAnswer}.</p>
+    `);    
+  }
+  	
+  $('.container').append(`
+          <ul>
+            <li class="question">Question ${questionCount +1} of 10</li>
+            <li class="score">Score ${scoreCount} of 10</li>
+          </ul>  
+          <button id="continue">Continue</button>  
+      </section>    
+  `);
+  questionCounter();
+}
+
+//function to increment the question counter when a user clicks on the continue button and determine how to move to the next question. If the questionCount is less than the length of the STORE array then show the next question otherwise show them the end of the quiz pannel.
+function continueToNextQuestion() {
+  $('.container').on('click', '#continue', function(event) {
+    if (questionCount < STORE.length) {
+      nextQuestion();
+      quizEngine();
+    } else {
+      showEndOfQuizPannel();
+    }
+  });
+}  
+
+// when the user gets to the end of the quiz show them their score and button to re-start the quiz.
+function showEndOfQuizPannel() {
+$('.container').html(`
+    <section role="region">
+      <img src="https://www.racingjunk.com/news/wp-content/uploads/2013/12/John-Force-Racing-Museum-003.jpg?w=222&h=166&crop=1" alt="Just a few of the Wally Trophies at the John Force Racing Museum" />
+        <p>You made it to the Winners Circle!</p>
+        <p>Out of the ${questionCount} questions, you got ${scoreCount} right. Click the button below to try the quiz again and improve your score!</p>
+        <button id="tryAgain">Try Again</button>  
+    </section>
+`);  
+}
+
+//upon clicking the Try Again button reload the page to bring the users back to the beginning and clear the question and score counters
+function tryTheQuizAgain() {
+  $('.container').on('click', '#tryAgain', function(event) {
+    window.location.reload();
+  });
+}
+
+// function to handle checking the users answer, moving to the next question and restarting the quiz
+function quizEngine() {
+  checkTheAnswer();
+  continueToNextQuestion();
+  tryTheQuizAgain();
+}
+
+//call back to start the quiz
+$(startQuiz);
---
+++ store.js
@@ -0,0 +1,133 @@
+const STORE = [
+    {
+      question: 'What does the acronym NHRA stand for?',
+      answerOptions: [
+        'New Hampshire Racing Authority',
+        'National Hot Rod Association',
+        'New Haven Racing Alliance',
+        'National Hemi Racer Accord'
+      ],
+      theRightAnswer: 'National Hot Rod Association',
+      rightAnswerPhrase: 'NHRA stands for ',
+      relatedPhoto: 'https://cdn.shopify.com/s/files/1/1706/5673/t/10/assets/nhra-logo.png?5095942340538535732',
+      alt: 'NHRA Logo'
+    },
+    {
+      question: 'What year was the NHRA established?',
+      answerOptions: [
+        '1947',
+        '1951',
+        '1968',
+        '1971'
+      ],
+      theRightAnswer: '1951',
+      rightAnswerPhrase: 'The NHRA was established in ',
+      relatedPhoto: 'https://bloximages.newyork1.vip.townnews.com/bakersfield.com/content/tncms/assets/v3/editorial/5/85/585f630a-b43d-11e7-aefc-d7914dd05b1d/59e7b03033893.image.jpg?resize=1200%2C945',
+      alt: 'Rick Stewart in his front engine mounted dragster'
+    },
+    {
+      question: 'Who is the founder of the NHRA?',
+      answerOptions: [
+        'Art Chrisman',
+        'Sydney Allard',
+        'Wally Parks',
+        'Gene Snow'
+      ],
+      theRightAnswer: 'Wally Parks',
+      rightAnswerPhrase: 'The founder of the NHRA is ',
+      relatedPhoto: 'https://www.getmoresports.com/wp-content/uploads/2016/08/page12_blog_entry23_summary_1.jpg',
+      alt: 'Artists rendering of NHRA Founder Wally Parks'
+    },
+    {
+      question: 'Every race is started once both drivers stage themselves. Drivers are laser focused on the image you see above. What is the name for the item in the image above?',
+      answerOptions: [
+      'the starting lights',
+      'the starting gun',
+      'the pumpkin',
+      'the christmas tree'
+      ],
+      theRightAnswer: 'the christmas tree',
+      rightAnswerPhrase: 'The marker that starts the race is known as ',
+      relatedPhoto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiQpPBGbdDhCdnyKlMYkoqxUZ7nc17_AM3BDrX-8_Toy8Xpxxlqw',
+      alt: 'NHRA Christmas Tree'
+    },
+    {
+      question: 'After each weekly event, the champion from each class receives a trophy. What is this trophy known as?',
+      answerOptions: [
+        'the stanley cup',
+        'the lombardi trophy',
+        'a wally',
+        'an emmy'
+      ],
+      theRightAnswer: 'a wally',
+      rightAnswerPhrase: 'The trophy is known as ',
+      relatedPhoto: 'https://motorracingpress.com/wp-content/uploads/2017/05/rcnhra25717.jpg',
+      alt: 'The Wally trophy.'
+    },
+    {
+      question: 'A major crash in 1970 led this driver to lose part of his foot. He rehabilitated himself and also invented the rear engine dragster which has been adopted as the standard in top fuel drag racing ever since. Who is this driver?',
+      answerOptions: [
+        'Don Garlits',
+        'Ed Hill',
+        'Gary Ormsby',
+        'Connie Kalita'
+      ],
+      theRightAnswer: 'Don Garlits',
+      rightAnswerPhrase: '',
+      relatedPhoto: 'http://www.roadkill.com/wp-content/uploads/2016/07/don-garlits-swamp-rat-13-rebuilt-17.jpg',
+      alt: 'Don Garlits crash in 1970.'
+    },
+    {
+      question: 'Which pair of drivers were known as "The Snake & The Mongoose"',
+      answerOptions: [
+        'John Force & Ron Capps',
+        'Billy Torrence & Steve Torrence',
+        'Don Prudhomme & Tom McEwen',
+        'Kenny Bernstien & Joe Amato'
+      ],
+      theRightAnswer: 'Don Prudhomme & Tom McEwen',
+      rightAnswerPhrase: '"The Snake & The Mongoose" are ',
+      relatedPhoto: 'http://www.route66raceway.com/~/media/91A257B3D4ED45CDA4FF5946BCCD666C.ashx?w=325',
+      alt: 'The Snake & The Mongoose'
+    },
+    {
+      question: 'Who is known as the "First Lady of Drag Racing"?',
+      answerOptions: [
+        'Brittany Force',
+        'Shirley Muldowney',
+        'Leah Pritchett',
+        'Erica Enders'
+      ],
+      theRightAnswer: 'Shirley Muldowney',
+      rightAnswerPhrase: 'The "First Lady of Drag Racing" is ',
+      relatedPhoto: 'https://motorlands.net/wp-content/uploads/2017/02/Shirley-Muldowney1-799x4451.jpg',
+      alt: 'The First Lady of Drag Racing in front of her funny car.'
+    },
+    {
+      question: 'The Force family is often known as the "First Family of Drag Racing". How many of John\'s daughters are actively competing on the NHRA circuit?',
+      answerOptions: [
+        'one',
+        'two',
+        'three',
+        'none'
+      ],
+      theRightAnswer: 'two',
+      rightAnswerPhrase: 'Courtney and Brittany are the only active ',
+      relatedPhoto: 'https://parade.com/wp-content/uploads/2016/06/John-Laurie-Courtney-Ashley-Brittany.jpg',
+      alt: 'The Force Family. John, Laurie, Courtney, Ashley and Brittany.'
+    },
+    {
+      question: 'Today\'s top fuel dragsters and funny cars are exceeding speeds of 300mph. What type of fuel are these machines using?',
+      answerOptions: [
+        'moonshine',
+        'nitromethane',
+        'regular unleaded 87',
+        'plutonium'
+      ],
+      theRightAnswer: 'nitromethane',
+      rightAnswerPhrase: 'Currently the fuel used and approved by the NHRA is ',
+      relatedPhoto: 'https://moparinsiders.com/wp-content/uploads/2018/07/PritchettMileHighQual-780x405.jpg',
+      alt: 'Leah Pritchett lights it up at night during a qualifying session.'
+    }
+
+];
